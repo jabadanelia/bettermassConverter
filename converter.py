7aeeb6db7ee9199685d008b82de8c6e32514d8fe
jaba danelia <jabadanelia86@gmail.com>
2023-10-08 00:00:46 +0400

    First commit

--- converter.py
+++ converter.py
@@ -1,16 +1,4 @@
-
-
-
-
-unit = input("T or KG? ")
 amount = float(input("Amount: "))
 
-if unit == "T":
-	print(f"{amount * 1000} Kg.")
+print(f"{amount * 1000} Kg.")
 
-elif unit == "KG":
-	print(f"{amount / 1000} Kg.")
-
-else:
-	print("Error!")
-	
